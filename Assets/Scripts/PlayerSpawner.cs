abd73141c4ce6e7ecc6020369d05122b462335f0
carcaAntoine <reseauantoine@gmail.com>
2023-03-15 14:02:29 +0100

    ajout projet

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    SOActorModel actorModel;
+    GameObject playerShip;
+
+    void Start()
+    {
+        CreatePlayer();
+    }
+
+    void CreatePlayer()
+    {
+        //CREATE PLAYER 
+        actorModel = Object.Instantiate(Resources.Load("Player_Default")) as SOActorModel;
+        playerShip = GameObject.Instantiate(actorModel.actor) as GameObject;
+        playerShip.GetComponent<Player>().ActorStats(actorModel); 
+
+        //SET PLAYER UP
+        playerShip.transform.rotation = Quaternion.Euler(0, 180, 0);
+        playerShip.transform.localScale = new Vector3(60, 60, 60);
+        playerShip.name = "Player";
+        playerShip.transform.SetParent(this.transform);
+        playerShip.transform.position = Vector3.zero;
+
+    }
+
+    
+}
