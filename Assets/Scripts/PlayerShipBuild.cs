15abe95d5274e811c3abbce49968f202be43e369
carcaAntoine <reseauantoine@gmail.com>
2023-03-24 14:30:04 +0100

    suite creation shop

--- Assets/Scripts/PlayerShipBuild.cs
+++ Assets/Scripts/PlayerShipBuild.cs
@@ -7,9 +7,28 @@ public class PlayerShipBuild : MonoBehaviour
     [SerializeField] GameObject[] shopButtons;
     GameObject target;
     GameObject tmpSelection;
+    GameObject textBoxPanel;
+
+    [SerializeField] GameObject[] visualWeapons;
+    [SerializeField] SOActorModel defaultPlayerShip;
+    GameObject playerShip;
+    GameObject buyButton;
+    GameObject bankObj;
+    GameObject shipItem;
+    int bank = 600;
+    bool purchaseMade = false;
+
     void Start()
     {
         TurnOffSelectionHighlights();
+        textBoxPanel = GameObject.Find("textBoxPanel");
+
+        purchaseMade = false;
+        bankObj = GameObject.Find("bank");
+        bankObj.GetComponentInChildren<TextMesh>().text = bank.ToString();
+        buyButton = textBoxPanel.transform.Find("BUY ?").gameObject;
+        TurnOffPlayerShipVisuals();
+        PreparePlayerShipForUpgrade();
     }
 
     void TurnOffSelectionHighlights()
@@ -49,6 +68,26 @@ public class PlayerShipBuild : MonoBehaviour
                 {
                     TurnOffSelectionHighlights();
                     Select();
+                    UpdateDescriptionBox();
+
+                    //Not already sold
+                    if(target.transform.Find("itemText").GetComponent<TextMesh>().text != "SOLD")
+                    {
+                        //can afford
+                        Affordable();
+
+                        //can not afford
+                        LackOfCredits();
+                    }
+                    else if(target.transform.Find("item Text").GetComponent<TextMesh>().text == "SOLD")
+                    {
+                        SoldOut();
+                    }
+                }
+                else if(target.name == "BUY ?")
+                {
+                    BuyItem();
+
                 }
             }
         }
@@ -58,5 +97,78 @@ public class PlayerShipBuild : MonoBehaviour
     {
         tmpSelection = target.transform.Find("SelectionQuad").gameObject;
         tmpSelection.SetActive(true);
+    }
+
+    void UpdateDescriptionBox()
+    {
+        textBoxPanel.transform.Find("name").gameObject.GetComponent<TextMesh>().text = tmpSelection.GetComponentInParent<ShopPiece>().ShopSelection.iconName;
+        textBoxPanel.transform.Find("desc").gameObject.GetComponent<TextMesh>().text = tmpSelection.GetComponentInParent<ShopPiece>().ShopSelection.description;
+    }
+
+    void Affordable()
+    {
+        if(bank >= System.Int32.Parse(target.transform.GetComponent<ShopPiece>().ShopSelection.cost))
+        {
+            Debug.Log("CAN BUY");
+            buyButton.SetActive(true);
+        }
+    }
+
+    void LackOfCredits()
+    {
+        if(bank < System.Int32.Parse(target.transform.Find("itemText").GetComponent<TextMesh>().text))
+        {
+            Debug.Log("CAN BUY");
+        }
+    }
+
+    void SoldOut()
+    {
+        Debug.Log("SOLD OUT");
+    }
+
+    void TurnOffPlayerShipVisuals()
+    {
+        for(int i = 0; i < visualWeapons.Length; i++)
+        {
+            visualWeapons[i].gameObject.SetActive(false);
+        }
+    }
+
+    void PreparePlayerShipForUpgrade()
+    {
+        playerShip = GameObject.Instantiate(defaultPlayerShip.actor);
+
+        playerShip.GetComponent<Player>().enabled = false;
+        playerShip.transform.position = new Vector3(0,10000,0);
+        playerShip.GetComponent<IActorTemplate>().ActorStats(defaultPlayerShip);
+    }
+
+    void BuyItem()
+    {
+        Debug.Log("PURCHASED");
+        purchaseMade = true;
+        buyButton.SetActive(false);
+        tmpSelection.SetActive(false);
+
+        for(int i = 0; i < visualWeapons.Length; i++)
+        {
+            if(visualWeapons[i].name == tmpSelection.transform.parent.gameObject.GetComponent<ShopPiece>().ShopSelection.iconName)
+            {
+                visualWeapons[i].SetActive(true);
+            }
+        }
+
+        UpgradeToShip(tmpSelection.transform.parent.gameObject.GetComponent<ShopPiece>().ShopSelection.iconName);
+        bank = bank - System.Int32.Parse(tmpSelection.transform.parent.GetComponent<ShopPiece>().ShopSelection.cost);
+        bankObj.transform.Find("bankText").GetComponent<TextMesh>().text = bank.ToString();
+        tmpSelection.transform.parent.transform.Find("itemText").GetComponent<TextMesh>().text = "SOLD";
+    }
+
+    void UpgradeToShip(string upgrade)
+    {
+        shipItem = GameObject.Instantiate(Resources.Load(upgrade)) as GameObject;
+        shipItem.transform.SetParent(playerShip.transform);
+        shipItem.transform.localPosition = Vector3.zero;
     }
 }
